80377aff009bbc1832e6aa145c1a9830270a9666
Emily <chane439@newschool.edu>
2018-10-30 11:16:25 -0400

    lab 5

---
+++ labs/lab 5/assets/js/app.js
@@ -0,0 +1,32 @@
+function hello_world () {
+    alert ('Hello World');
+}
+hello_world();
+
+
+
+
+
+$('.nav-item').click(function(){
+    var button_name = $(this).data('button');
+    
+    $ ('.about-text').removeClass('hide');  
+    $ ('.nav-item').removeClass('active');  
+    $('.content').attr('class','column content');
+    $(this).addClass('active'); 
+    $('.content').addClass(button_name);
+
+ if (button_name == 'beyonce') {
+    $('.slay').removeClass('hide');
+    $('audio')[0].play();
+ } else if (button_name == 'about'){
+    $ ('.about-text').removeClass('hide');    
+ }
+
+});
+
+$('.close').click(function(){
+  $('.slay').addClass('hide');
+  $('audio')[0].pause();
+  $('.nav-item').removeClass ('active');
+});
